0b791b65af3d31886064417ac691316b3198f8d7
Kevin Kim <hckim@invesume.com>
2020-11-05 12:01:35 +0900

    Added postinst.sh in preseed dir

---
+++ resource/extract-cd/preseed/postinst.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# firewall on
+sudo sed -i 's/ENABLED=no/ENABLED=yes/g' /target/etc/ufw/ufw.conf
+
+# add pkgs
+chroot /target sudo apt update
+chroot /target sudo apt install -y gparted unalz
+chroot /target sudo apt install -y google-chrome-stable
+
+# reset machin-id
+cat /dev/null | sudo tee /target/etc/machine-id
+
+# add installed user count
+RD=`head -c 16 /dev/urandom | md5sum | head -c 32`
+CODE="jin"
+VER="4.0"
+curl -XPOST "http://pms.invesume.com:8086/write?db=hamonikr" --data-binary "machineid,codename=$CODE,version=$VER value=\"$RD\""
