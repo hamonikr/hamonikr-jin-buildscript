cb121e3140f64748409477431f94bf82e03c37ee
Kevin Kim <hckim@invesume.com>
2020-11-06 10:41:27 +0900

    change postinst.sh

--- resource/extract-cd/preseed/postinst.sh
+++ resource/extract-cd/preseed/postinst.sh
@@ -4,9 +4,9 @@
 sudo sed -i 's/ENABLED=no/ENABLED=yes/g' /target/etc/ufw/ufw.conf
 
 # add pkgs
-chroot /target sudo apt update
-chroot /target sudo apt install -y gparted unalz
-chroot /target sudo apt install -y google-chrome-stable
+#chroot /target sudo apt update
+#chroot /target sudo apt install -y gparted unalz
+#chroot /target sudo apt install -y google-chrome-stable
 
 # reset machin-id
 cat /dev/null | sudo tee /target/etc/machine-id
